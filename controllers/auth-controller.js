4a91ed3d29f9bf9d80efdcba79c80d9ae65a8975
Khushxal <vkkhushal18@gmail.com>
2024-05-24 14:32:34 +0530

    changes

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -17,6 +17,7 @@ async function Register(req, res) {
     console.log(req.body);
     const { username, phone, password, isAdmin } = req.body; // derefernce the array
     const userExist = await User.findOne({ username });
+    let encryptedPassword = bycrypt.hash(password, 10);
     if (userExist) {
       res.send("<h1>User already exists please sign in to continue</h1>");
     } else {
@@ -24,7 +25,7 @@ async function Register(req, res) {
       const addUser = await User.create({
         username,
         phone,
-        password,
+        password: encryptedPassword,
         isAdmin,
       });
       res.send("<h1>User Registered Please login </h1>");
