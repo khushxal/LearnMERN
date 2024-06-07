608d8572276ba0823de40eabcd808e799a87470a
khushxal <vkkhushal18@gmail.com>
2024-06-07 21:54:51 +0530

    changes

--- mern_app1_app/src/pages/Books.jsx
+++ mern_app1_app/src/pages/Books.jsx
@@ -1,7 +1,12 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { UseAuth } from "../auth/auth";
+import { useNavigate } from "react-router-dom";
 
 function Books() {
+  const navigate = useNavigate();
+  const { userData } = UseAuth();
+
   const URL = "http://localhost:3001/api/data/books";
 
   const [booksList, setBooksList] = useState([]);
@@ -26,34 +31,38 @@ function Books() {
 
   return (
     <div className="container">
-      <div className="row mb-3 fs-4">
-        {loading ? (
-          <div>{loading}</div>
-        ) : (
-          <div className="grid-container">
-            {booksList.map((book, index) => (
-              <div className="col card" key={index}>
-                <div className="card-img text-center">
-                  <img
-                    src={book.imgUrl}
-                    alt="Cover page"
-                    height={100}
-                    width={100}
-                  />
-                </div>
-                <div className="card-body">
-                  <div className="fs-4 fw-bold mb-4"> {book.title}</div>
-                  <div className="fs-5 fw-bolder mb-2">{book.publisher}</div>
-                  <div className="fs-6 fw-bold mb-2">
-                    Author : {book.authors}
+      {userData ? (
+        <div className="row mb-3 fs-4">
+          {loading ? (
+            <div>{loading}</div>
+          ) : (
+            <div className="grid-container">
+              {booksList.map((book, index) => (
+                <div className="col card" key={index}>
+                  <div className="card-img text-center">
+                    <img
+                      src={book.imgUrl}
+                      alt="Cover page"
+                      height={100}
+                      width={100}
+                    />
                   </div>
-                  <div className="fs-6 ">Published : {book.year}</div>
+                  <div className="card-body">
+                    <div className="fs-4 fw-bold mb-4"> {book.title}</div>
+                    <div className="fs-5 fw-bolder mb-2">{book.publisher}</div>
+                    <div className="fs-6 fw-bold mb-2">
+                      Author : {book.authors}
+                    </div>
+                    <div className="fs-6 ">Published : {book.year}</div>
+                  </div>
                 </div>
-              </div>
-            ))}
-          </div>
-        )}
-      </div>
+              ))}
+            </div>
+          )}
+        </div>
+      ) : (
+        navigate("/login")
+      )}
     </div>
   );
 }
--- mern_app1_server/controllers/book-controller.js
+++ mern_app1_server/controllers/book-controller.js
@@ -3,7 +3,6 @@ import Book from "../models/book-model.js";
 async function books(req, res, next) {
   try {
     const response = await Book.find();
-    console.log(response);
     res.status(200).json({ books: response });
   } catch (error) {
     console.log(error);
