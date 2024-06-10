8d53e62109beee22a364d100e411ff54b6e636d8
Khushxal <vkkhushal18@gmail.com>
2024-06-10 18:54:03 +0530

    changes

--- mern_app1_app/src/pages/Admin.jsx
+++ mern_app1_app/src/pages/Admin.jsx
@@ -1,7 +1,25 @@
 import React from "react";
 
 function Admin() {
-  return <div></div>;
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-3 card me-2">
+          <div className="mt-2 fs-2">Radhe Radhe Admin</div>
+          <hr />
+          <div>
+            <ul className="fs-4">
+              <li>Admin Profile</li>
+              <li>Users</li>
+              <li>Queries</li>
+              <li>Books</li>
+            </ul>
+          </div>
+        </div>
+        <div className="col-8 card"></div>
+      </div>
+    </div>
+  );
 }
 
 export default Admin;
