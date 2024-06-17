28307e189c4358483683b0765ab6067de38fc948
Khushxal <vkkhushal18@gmail.com>
2024-06-17 14:31:23 +0530

    Changes

--- mern_app1_app/src/pages/Logout.jsx
+++ mern_app1_app/src/pages/Logout.jsx
@@ -1,12 +1,18 @@
 import { useEffect } from "react";
 import { UseAuth } from "../auth/auth";
 import { Navigate } from "react-router-dom";
+import { toast } from "react-toastify";
 function Logout() {
   const { deleteToken } = UseAuth();
   useEffect(function () {
     deleteToken();
+    toast.success("Logged out successfully");
   }, []);
-  return <Navigate to={"/login"}></Navigate>;
+  return (
+    <div>
+      <Navigate to={"/login"}></Navigate>;
+    </div>
+  );
 }
 
 export default Logout;
