ab358f7fbc16ce146177e592ca2f5bea6cfcdb03
Khushxal <vkkhushal18@gmail.com>
2024-07-26 17:40:56 +0530

    Changes in about

--- mern_app1_client/src/pages/About.jsx
+++ mern_app1_client/src/pages/About.jsx
@@ -11,7 +11,7 @@ function About() {
           <h1 className="mt-2 ms-2 text-center">About Developer</h1>
           <div className="col-7 fw-light">
             <p className="mt-2 ms-2 fs-3">
-              Radhe Radhe {user ? user.username : ""} ❤,
+              Radhe Radhe {user ? user.username : ""} ,
             </p>
             <p className="mt-2 ms-2 fs-4">
               This is me (Khushal Verma). Hope you love the website created by
